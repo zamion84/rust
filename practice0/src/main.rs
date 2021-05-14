cddc995b1ee8a1539c0234800b68d523a5d9d50a
root <root@DESKTOP-OPUTBLE.localdomain>
2021-05-13 23:08:21 -0500

    practice_added1

--- practice0/src/main.rs
+++ practice0/src/main.rs
@@ -1,9 +1,27 @@
+use std::io;
+//use std::string;
+//aim for this practice is to intake some text and output the length with error handling
 fn main() {
-    let output = first(0);
-    println!("{}", output);
-}
-fn first(x: i32) -> i32 {
-    let a = 32;
-    x + a
+    
+    //let input = " ";
+    let mut varstr = String::from("");
+    io::stdin()
+        .read_line(&mut varstr)
+        .expect("Failed to read line");
 
+    let varstr: String =! varstr
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+    
+        let output = first(varstr);
+    
+        println!("{}", output);
+            
 }
+fn first(x: String) -> i32 {
+    let a = 32;
+    (x.len() + a)
+}
+
+
